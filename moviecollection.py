8a3b48d9b68fd8a25858b88edc350d38b76342d6
luise <luiserivaldo99@gmail.com>
2020-01-21 18:22:14 +0800

    create moviecollection.py file

---
+++ moviecollection.py
@@ -0,0 +1,28 @@
+"""
+Assessment 2
+by Luise Rivaldo
+MovieCollection class
+"""
+
+from movie import Movie
+
+class MovieCollection(Movie):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def add_movie:
+
+    def get_movie:
+
+    def load_movie:
+
+    def save_movie:
+
+    def sort_movie:
+
+
+# add movie – add a single Movie object to the movies attribute
+# get number of watched movies
+# load movies (from csv file into Movie objects in the list)
+# save movies (from movie list into csv file)
+# sort (by the key passed in, then by title)
